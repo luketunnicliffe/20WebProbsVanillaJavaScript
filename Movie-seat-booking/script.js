ff1b4c51f46eeed313b9e62b09307d72a32eca01
tekipakicoder <luke@eigowithluke.com>
2021-01-16 00:15:43 +0900

    Finished movie theatre

--- Movie-seat-booking/script.js
+++ Movie-seat-booking/script.js
@@ -4,12 +4,38 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+
 let ticketPrice = +movieSelect.value;
+populateUI()
+//Save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
+//Get data from local storage and populate UI.
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index)=>{
+            if(selectedSeats.indexOf(index)> -1){
+                seat.classList.add('selected')
+
+        }
+    });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+    }
 
 //Movie select event
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
@@ -45,3 +71,5 @@ container.addEventListener('click', (e) => {
     }
     })
 
+    // Initial count and total set
+    updateSelectedCount();
